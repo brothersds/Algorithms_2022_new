4e450e9604a45fa1285658eb15957d856fe2b84f
Dmitry <brothersds@ngs.ru>
2022-05-02 15:29:23 +0300

    end task1
    end task_1

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -2,4 +2,12 @@
 Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 https://drive.google.com/file/d/1xpdjGb4dnycOuNAA8Te81iUPAOYEXlcV/view?usp=sharing
 """
-
+print("Поиск суммы и произведения чисел трехзначного числа")
+num_user = int(input("Введите трехзначное число: "))
+num_one = num_user % 10
+num_two = (num_user % 100) // 10
+num_three = (num_user // 100)
+sum_num = (num_one + num_two + num_three)
+mul_num = (num_one * num_two * num_three)
+print(f'Сумма чисел равна: {sum_num}')
+print(f'Произведение чисел равно: {mul_num}')
