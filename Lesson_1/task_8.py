294b50eefb61bd5e3e4453ff7833c9d847562951
Dmitry <brothersds@ngs.ru>
2022-05-02 23:28:36 +0300

    end task_8
    task_8

--- Lesson_1/task_8.py
+++ Lesson_1/task_8.py
@@ -1,6 +1,10 @@
 """
 Определить, является ли год, который ввел пользователь, високосным или не високосным.
-
+https://drive.google.com/file/d/1xpdjGb4dnycOuNAA8Te81iUPAOYEXlcV/view?usp=sharing
 """
 year_user = int(input("Введите год: "))
 
+if year_user % 400 == 0 or (year_user % 100 != 0 and year_user % 4 == 0):
+    print(f'Год {year_user} високосный')
+else:
+    print(f'Год {year_user} не високосный')
