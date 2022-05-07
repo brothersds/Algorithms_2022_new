a45e666016acb28d8efbf307c55f36c721442371
Dmitry <brothersds@ngs.ru>
2022-05-08 00:09:47 +0300

    end task_2

--- Lesson_2/L2_task_2.py
+++ Lesson_2/L2_task_2.py
@@ -1,16 +1,22 @@
 """
 Посчитать четные и нечетные цифры введенного натурального числа. Например, если введено число 34560, в нем 3 четные
 цифры (4, 6 и 0) и 2 нечетные (3 и 5).
-
+https://drive.google.com/file/d/1dwhQvcBzeoUEK2onaSP5wYbwg6QopT7-/view?usp=sharing
 """
 
 
-def res(a, b):
-    if a == b:
-        return a
-    if a > b:
-        return res(a // 10, b)
+def calc(a):
+    k1 = k2 = 0
+    while a > 0:
+        if a % 2 == 0:
+            k2 += 1
+        else:
+            k1 += 1
+        a = a // 10
+    return f'нечетных чисел: {k1} четных чисел: {k2}'
 
 
-num_user = int(input("Введите целое вещественное число: "))
-print(res(num_user, 0))
+num_user = int(input("Введите целое натуральное число: "))
+res = calc(num_user)
+print(res)
+
