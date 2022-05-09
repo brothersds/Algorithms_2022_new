9d9cac2ba8cacd7488055ec70d6db3cab3cb42f0
Dmitry <brothersds@ngs.ru>
2022-05-09 18:45:48 +0300

    end task_3

--- Lesson_3/L3_task_3.py
+++ Lesson_3/L3_task_3.py
@@ -5,7 +5,22 @@
 import random
 
 SIZE = 10
-MIN_ITEM = 0
+MIN_ITEM = -100
 MAX_ITEM = 100
 ls = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(ls)
+
+# решение задачи
+min_num = max_num = ls[0]
+index_min = 0
+index_max = 0
+en_ls = enumerate(ls)
+for index, item in en_ls:
+    if item < min_num:
+        index_min = index
+        min_num = item
+    elif item > max_num:
+        index_max = index
+        max_num = item
+ls[index_min], ls[index_max] = ls[index_max], ls[index_min]
 print(ls)
