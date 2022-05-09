bbc71a58bc92cee04a92c0ff5f3071047f618b7f
Dmitry <brothersds@ngs.ru>
2022-05-09 21:23:13 +0300

    end task 8

--- Lesson_3/L3_task_8.py
+++ Lesson_3/L3_task_8.py
@@ -3,3 +3,26 @@
 введенных элементов каждой строки и записывать ее в последнюю ячейку строки. В конце следует вывести полученную
 матрицу.
 """
+
+# постановка задачи
+
+# решение задачи
+row = 5
+col = 4
+matrix = []
+for i in range(row):
+    rows = []
+    sum_ = 0
+
+    for j in range(col - 1):
+        num = int(input(f'{i}-я строка, {j}-й элемент : '))
+        sum_ += num
+        rows.append(num)
+
+    rows.append(sum_)
+    matrix.append(rows)
+
+for line in matrix:
+    for item in line:
+        print(f'{item:>10_}', end='')
+    print()
