c1f7bd176fc96e948e7b659e416dc4167ed5020c
Dmitry <brothersds@ngs.ru>
2022-05-09 18:58:17 +0300

    statement task_4

--- Lesson_3/L3_task_4.py
+++ Lesson_3/L3_task_4.py
@@ -1,3 +1,13 @@
 """
 Определить, какое число в массиве встречается чаще всего.
 """
+# постановка задачи
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 5
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+
+# решение задачи
