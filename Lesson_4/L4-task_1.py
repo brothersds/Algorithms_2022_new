5f13f44e661dc55b0d70d7a7ce3084171900ec57
Dmitry <brothersds@ngs.ru>
2022-05-13 01:40:03 +0300

    continue task 1

--- Lesson_4/L4-task_1.py
+++ Lesson_4/L4-task_1.py
@@ -8,29 +8,75 @@
 ● результаты анализа вставить в виде комментариев в файл с кодом (не забудьте указать, для каких N вы проводили замеры),
 ● написать общий вывод: какой из трёх вариантов лучше и почему.
 
-Выбрана задача Lesson 2 task 5
-Вывести на экран коды и символы таблицы ASCII, начиная с символа под номером 32 и заканчивая 127-м включительно.
-Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
+Выбрана задача Lesson 3 task 4 ()
+
+Определить, какое число в массиве встречается чаще всего.
 """
 
 # постановка задачи
+import random
+
+SIZE = 500
+MIN_ITEM = 0
+array = [random.randint(MIN_ITEM, SIZE // 2) for _ in range(SIZE)]
+print(array)
+
+# решение задачи рекурсивным методом
 
 
-def calc(a, b):
-    if a == b:
-        return f'{a} is {chr(a)}'
-    if a < b:
-        return f'{a} is {chr(a)}, {calc(a + 1, b)}'
+def dict_rec(arr, res={}, repeat=[1], item=[]):
+    if not arr:
+        return
+    else:
+        first_el = arr[0]
+        if not type(first_el) == list:
+            res[first_el] = res.get(first_el, 0) + 1
+            if repeat[len(repeat) - 1] < res[first_el]:
+                item.clear(), item.append(first_el)
+                repeat.clear(), repeat.append(res[first_el])
+        dict_rec(arr[1:])
+    return item, repeat
 
 
-n = 32
-step = 0
-for i in range(32, 127):
-    if i == n:
-        if i == 122:
-            step = 5
-        else:
-            step = 9
-        res = calc(i, i + step)
-        print(res)
-        n += 10
+result = dict_rec(array)
+if result is None:
+    print("Нет совпадений")
+else:
+    print(f'Число {result[0][0]} встречается {result[1][0]} раз(-a)')
+
+# решение задачи циклом for
+count = {}
+repeat = 1
+num = None
+for item in array:
+    if item in count:
+        count[item] += 1
+    else:
+        count[item] = 1
+    if count[item] > repeat:
+        repeat = count[item]
+        num = item
+if num is None:
+    print("Нет совпадений")
+else:
+    print(f'Число {num} встречается {repeat} раз(-a)')
+
+# решение задачи циклом while
+count = {}
+repeat = 1
+n = 0
+num = 0
+while n < len(array):
+    i = array[n]
+    n += 1
+    if i in count:
+        count[i] += 1
+    else:
+        count[i] = 1
+    if count[i] > repeat:
+        repeat = count[i]
+        num = i
+if num is None:
+    print("Нет совпадений")
+else:
+    print(f'Число {num} встречается {repeat} раз(-a)')
