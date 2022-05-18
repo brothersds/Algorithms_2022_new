14623c07a55588b37c45814898ac01fc4c5aea5b
Dmitry <brothersds@ngs.ru>
2022-05-18 23:38:08 +0300

    end task_1

--- Lesson_5/L5-task_1.py
+++ Lesson_5/L5-task_1.py
@@ -5,36 +5,51 @@
 """
 from collections import namedtuple
 
-NAME_1 = 'roga'
-NAME_2 = 'kopyta'
-INCOME_Q1_1 = 1000
-INCOME_Q2_1 = 1000
-INCOME_Q3_1 = 1000
-INCOME_Q4_1 = 1000
-INCOME_Q1_2 = 2000
-INCOME_Q2_2 = 2000
-INCOME_Q3_2 = 2000
-INCOME_Q4_2 = 2000
+
+def factory_input(number, factory_array=[]):
+    total_income = 0
+    i = 0
+    while True:
+        if i == number:
+            break
+        else:
+            factory_name = input(f'Введите название {i + 1}-го предприятия: ')
+            income_q1 = int(input("Введите прибыль за первый картал: "))
+            income_q2 = int(input("Введите прибыль за второй картал: "))
+            income_q3 = int(input("Введите прибыль за третий картал: "))
+            income_q4 = int(input("Введите прибыль за четвертый картал: "))
+            income_year = income_q1 + income_q2 + income_q3 + income_q1
+            total_income += income_year
+            factory_array.append(Factory(factory_name=factory_name, income_q1=income_q1, income_q2=income_q2,\
+                                         income_q3=income_q3, income_q4=income_q4, income_year=income_year))
+            i += 1
+    return total_income, factory_array
+
+
+def total_average(total, quanity):
+    total_avg = total // quanity
+    return total_avg
+
+
+def factory_income_upper_avg(arr, total_avg, array_up=[]):
+    for Factory in arr:
+        if Factory.income_year >= total_avg:
+            array_up.append(Factory.factory_name)
+    return array_up
+
+
+def factory_income_lower_avg(arr, total_avg, array_low=[]):
+    for Factory in arr:
+        if Factory.income_year < total_avg:
+            array_low.append(Factory.factory_name)
+    return array_low
+
 
 factory_calc = int(input("Введите кол-во предприятий: "))
-Factory = namedtuple('Factory', 'name income_q1 income_q2 income_q3 income_q4 income_year')
-
-array_data = []
-total_income = 0
-
-while True:
-    if factory_calc == 0:
-        break
-    else:
-        factory_name = input("Введите название предприятия: ")
-        income_q1 = int(input("Введите прибыль за первый картал: "))
-        income_q2 = int(input("Введите прибыль за второй картал: "))
-        income_q3 = int(input("Введите прибыль за третий картал: "))
-        income_q4 = int(input("Введите прибыль за четвертый картал: "))
-        income_year = income_q1 + income_q2 + income_q3 + income_q1
-        total_income += income_year
-        array_data.append(Factory(factory_name, income_q1, income_q2, income_q3, income_q4, income_year))
-        factory_calc -= 1
-
-
-print(array_data)
+Factory = namedtuple('Factory', 'factory_name income_q1 income_q2 income_q3 income_q4 income_year')
+array = factory_input(factory_calc)
+income_avg = total_average(array[0], factory_calc)
+income_upper_avg = factory_income_upper_avg(array[1], income_avg)
+income_lower_avg = factory_income_lower_avg(array[1], income_avg)
+print(f'Компании с прибылью выше средней {income_avg}: {income_upper_avg}')
+print(f'Компании с прибылью ниже средней {income_avg}: {income_lower_avg}')
