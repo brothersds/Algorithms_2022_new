1d7470e1e5bc70ba88b47ff47fc053d8d120aa8b
Dmitry <brothersds@ngs.ru>
2022-05-21 22:46:28 +0300

    continue task_1

--- Lesson_6/L6-task_1.py
+++ Lesson_6/L6-task_1.py
@@ -2,3 +2,37 @@
 Подсчитать, сколько было выделено памяти под переменные в ранее разработанных программах в рамках первых трех уроков.
 Проанализировать результат и определить программы с наиболее эффективным использованием памяти.
 """
+
+from collections import deque
+
+
+def func_hex_sum(x, y):
+    hex_dec = dict(
+        zip((list([str(i) for i in range(0, 10, 1)]) + list('ABCDEF')), list([str(i) for i in range(0, 16, 1)])))
+    dec_hex = dict(
+        zip(list([str(i) for i in range(0, 16, 1)]), (list([str(i) for i in range(0, 10, 1)]) + list('ABCDEF'))))
+    res_sum = deque()
+    if len(x) > len(y):
+        x, y = deque(x), deque(y)
+    else:
+        y, x = deque(x), deque(y)
+    transfer_number = 0
+    for i in range(len(x)):
+        if not y:
+            res = int(hex_dec[x.pop()]) + transfer_number
+        else:
+            res = int(hex_dec[x.pop()]) + int(hex_dec[y.pop()]) + transfer_number
+        transfer_number = 0
+        if res >= 16:
+            res_sum.appendleft(dec_hex[str(res - 16)])
+            transfer_number = 1
+        else:
+            res_sum.appendleft(dec_hex[str(res)])
+    if transfer_number == 1:
+        res_sum.appendleft('1')
+    return list(res_sum)
+
+
+a = list(input('Введите 1-е шестнадцатиричное число: ').upper())
+b = list(input('Введите 2-е шестнадцатиричное число: ').upper())
+print('Сумма чисел равна: ', f'{func_hex_sum(a, b)}')
